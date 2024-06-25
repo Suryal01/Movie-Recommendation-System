d34cafed63aedb99211c976bc5f5cf0a11e2895f
Suryal01 <157194240+Suryal01@users.noreply.github.com>
2024-06-25 15:20:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+import pickle
+
+import _pickle as cPickle  # for unloading the compressed pickle file
+import bz2file as bz2  # compressing the pickle file which is greater than 25mb for deployment on github and streamlit
+import requests
+import streamlit as st
+
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names,recommended_movie_posters
+
+
+st.header('Movie Recommender System')
+movies = pickle.load(open('model\movie_list.pkl','rb'))
+similarity_original = pickle.load(open('model\similarity.pkl','rb'))
+# ----------------------------------------------------------------------------------------------
+
+# create a compressed model file which can be deployed on streamlit via github
+# https://srivastavprojyot.medium.com/compress-large-ml-pickle-files-for-deployment-6f6afe315380
+
+def compressed_pickle(title, data):
+
+    with bz2.BZ2File(title + '.pbz2', 'w') as f:
+        pickle.dump(data, f)
+
+compressed_pickle('similarity-pkl-compressed', similarity_original)
+
+# Load any compressed pickle file
+def decompress_pickle(file):
+ data = bz2.BZ2File(file, 'rb')
+ data = cPickle.load(data)
+ return data
+
+similarity = decompress_pickle('similarity-pkl-compressed.pbz2')
+
+# compress_similarity = pickle.load(open('similarity-pkl-compressed.pbz2','rb'))
+
+# ----------------------------------------------------------------------------------------------
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
